24d9acc29f393c60ccea0583da1622f28f6ffb3e
vallarasusf4653 <166098572+vallarasusf4653@users.noreply.github.com>
2024-04-30 13:54:51 +0530

    Adding Calculator-App folder

---
+++ Calculator-App/JS/script.js
@@ -0,0 +1,7 @@
+function result()
+{
+    var dispay=document.getElementsByClassName("dispaly").value;
+    var result = display.value=eval(display.value);
+    document.getElementsByClassName("display").innerItem=result;
+}
+ 
